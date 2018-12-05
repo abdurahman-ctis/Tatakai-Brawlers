503c422117f072b68e2ad3338ff01d4e68e2a6f4
aboudicheng <aboudicheng@gmail.com>
2018-12-05 23:27:37 +0300

    Edit Brawler Class

--- src/Brawler.java
+++ src/Brawler.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,23 @@
  * @author Cheng
  */
 public abstract class Brawler {
+    protected String name;
+    protected int hp;
+    protected String img;
+    protected ArrayList<Power> powerList = new ArrayList();
+
+    public Brawler(String name, int hp, String img, ArrayList<Power> powerList) {
+        this.name = name;
+        this.hp = hp;
+        this.img = img;
+        this.powerList = powerList;
+    }
+    
+    public abstract void attack(Power p);
+    
+    @Override
+    public String toString() {
+        return "\nName: " + name + "\nHP: " + hp + "\n";
+    }
     
 }
